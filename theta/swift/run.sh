12131bc500d382e087589a2b7c41eb4bb2894d98
Matthieu Dorier <mdorier@anl.gov>
2020-01-24 16:49:07 +0000

    changing number of procs in swift workflow

--- theta/swift/run.sh
+++ theta/swift/run.sh
@@ -1,7 +1,8 @@
 export PROJECT=radix-io
 export QUEUE=debug-cache-quad
 export WALLTIME=00:10:00
-export PPN=2
+export PPN=1
+export PROCS=3
 
 TURBINE_DIRECTIVE="module load intelpython36/2019.3.075 
 		   spack load -r flamestore" 
