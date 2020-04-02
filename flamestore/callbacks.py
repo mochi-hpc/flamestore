fef58c42bc5dd3e7d676c6d9abf37fad8401cb9f
Matthieu Dorier <mdorier@anl.gov>
2020-04-02 15:19:43 +0100

    added callback

--- flamestore/callbacks.py
+++ flamestore/callbacks.py
@@ -1,9 +1,6 @@
 from keras.callbacks import Callback
 from keras import backend as K
-from . import ops
-from . import util
-import logging
-from .decorators import trace
+import json
 
 class RemoteCheckpointCallback(Callback):
     """Callback class that periodically checkpoints
@@ -12,23 +9,28 @@ class RemoteCheckpointCallback(Callback):
 
     def __init__(self,
                  model_name,
-                 client,
-                 provider_addr,
-                 provider_id=0,
+                 workspace,
+                 frequency={'epoch': 1},
                  include_optimizer=True,
-                 frequency={'epoch': 1}):
+                 restart=False,
+                 client=None,
+                 engine=None):
         """Constructor of RemoteCheckpointCallback.
 
         Args:
             model_name (string): name of the model.
-            client (flamestore.client.Client):
-                client to use to communicate with the provider.
-            provider_addr (string): provider address.
-            provider_id (int): provider id.
-            include_optimizer (bool):
-                whether to checkpoint the optimizer state.
+            workspace (string): path to the workspace.
             frequency (dict):
                 when to checkpoint the model (and possibly the optimizer).
+            include_optimizer (bool):
+                whether to checkpoint the optimizer state.
+            restart (bool):
+                whether to restart from an existing model.
+            client (flamestore.client.Client):
+                client to use to communicate with the provider
+                (if None, this class will initialize a client).
+            engine (pymargo.core.Engine):
+                Margo instance (if None, this class will instantiate one).
 
         Notes:
             * The frequency dictionary may provide the following keys:
@@ -38,16 +40,29 @@ class RemoteCheckpointCallback(Callback):
               a ValueError exception will be raised when training starts.
         """
         super()
-        self.logger            = logging.getLogger(type(self).__name__)
-        self.logger.debug('Creating a RemoteCheckpointCallback object')
-        self.model_name        = model_name
-        self.client            = client
-        self.provider_addr     = provider_addr
-        self.provider_id       = provider_id
-        self.include_optimizer = include_optimizer
-        self.frequency         = frequency
+        self._model_name        = model_name
+        self._client            = client
+        self._include_optimizer = include_optimizer
+        self._frequency         = frequency
+        self._restart           = restart
+        self._engine            = engine
+        self._workspace         = workspace
+        self._owns_engine       = False
+        self._owns_client       = False
+        if(self._engine is None and self._client is None):
+            self._owns_engine = True
+            import pymargo
+            import pymargo.core
+            with open(self._workspace+'/.flamestore/config.json') as f:
+                config = json.loads(f.read())
+                protocol = config['protocol']
+                self._engine = pymargo.core.Engine(protocol, 
+                        use_progress_thread=True, mode=pymargo.server)
+        if(self._client is None):
+            self._owns_client = True
+            from .client import Client
+            self._client = Client(self._engine, self._workspace)
 
-    @trace
     def on_train_begin(self, logs={}):
         """Callback method called when training begins.
 
@@ -55,64 +70,27 @@ class RemoteCheckpointCallback(Callback):
         initializes the Tensorflow operations required to send
         the model's and optimizer's tensors periodically.
         """
-        # register the model
-        provider = self.client.lookup(self.provider_addr, self.provider_id)
-        status, message = self.client.register_model(
-            provider,
-            self.model_name,
-            self.model,
-            self.include_optimizer)
-        if(status != 0):
-            self.logger.critical('register_model error: %s', message)
-            raise ValueError(message)
-        # compute model signature
-        sig = util._compute_model_signature(self.model)
-        # list model tensors
-        model_tensors = []
-        for l in self.model.layers:
-            for w in l.weights:
-                model_tensors.append(w)
-        self.logger.debug('Creating write_model TensorFlow operation')
-        # create a new operation to transfer the data
-        self.__write_model_op = ops._write_model(
-            client=self.client._get_id(),
-            provider_addr=self.provider_addr,
-            provider_id=self.provider_id,
-            model_name=self.model_name,
-            model_signature=sig,
-            tensors=model_tensors)
-        if self.include_optimizer:
-            # compute optimizer signature
-            sig = util._compute_optimizer_signature(self.model.optimizer)
-            # list optimizer tensors
-            optimizer_tensors = []
-            for w in self.model.optimizer.weights:
-                optimizer_tensors.append(w)
-            # create a new operation to transfer the data
-            self.logger.debug('Creating write_optimizer TensorFlow operation')
-            self.__write_optimizer_op = ops._write_optimizer(
-                client=self.client._get_id(),
-                provider_addr=self.provider_addr,
-                provider_id=self.provider_id,
-                model_name=self.model_name,
-                optimizer_signature=sig,
-                tensors=optimizer_tensors)
+        if(not self._restart):
+            self._client.register_model(self._model_name, self.model, include_optimizer=self._include_optimizer)
+        else:
+            self._client.load_weights(self._model_name, self.model, self._include_optimizer)
 
-    @trace
     def on_train_end(self, logs={}):
         """Callback method called when training finishes.
 
         This method destroyes the Tensorflow operations
         used to checkpoint tensors.
         """
-        del self.__write_model_op
-        del self.__write_optimizer_op
+        if(self._owns_client):
+            del self._client
+            self._client = None
+        if(self._owns_engine):
+            del self._engine
+            self._engine = None
 
-    @trace
     def on_epoch_begin(self, epoch, logs={}):
         """Callback method called when an epoch starts."""
 
-    @trace
     def on_epoch_end(self, epoch, logs={}):
         """Callback method called when an epoch ends.
 
@@ -120,25 +98,19 @@ class RemoteCheckpointCallback(Callback):
         """
         if 'epoch' in self.frequency:
             if epoch % self.frequency['epoch'] == 0:
-                self.logger.debug('Running write_model operation')
-                self.__write_model_op.run(session=K.get_session())
-                if self.include_optimizer:
-                    self.logger.debug('Running write_optimizer operation')
-                    self.__write_optimizer_op.run(session=K.get_session())
+                self._client.save_weights(self._model_name,
+                        self.model,
+                        include_optimizer=self._include_optimizer)
 
-    @trace
     def on_batch_begin(self, batch, logs={}):
         """Callback method called when a batch begins."""
 
-    @trace
     def on_batch_end(self, batch, logs={}):
         """Callback method called when a batch ends.
 
         This method may checkpoint the model and optimizer."""
         if 'batch' in self.frequency:
             if batch % self.frequency['batch'] == 0:
-                self.logger.debug('Running write_model operation')
-                self.__write_model_op.run(session=K.get_session())
-                if self.include_optimizer:
-                    self.logger.debug('Running write_optimizer operation')
-                    self.__write_optimizer_op.run(session=K.get_session())
+                self._client.save_weights(self._model_name,
+                        self.model,
+                        include_optimizer=self._include_optimizer)
